34f3b56c03eb32f888681b68290749137e5b2091
Kayla <6lackmacaroon@gmail.com>
2019-06-17 14:18:53 -0600

    built repair test per walkthrough

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,34 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
+
+
+describe('enhancer', () => {
+      describe('repair()', () => {
+            it('restores durability to 100', () => {
+                  expect(repair({ durability: 89 }).durability).toBe(100)
+                  expect(repair({ durability: -89 }).durability).toBe(100)
+                  expect(repair({ durability: 100 }).durability).toBe(100)
+                  expect(repair({ durability: 0 }).durability).toBe(100)
+            })
+      });
+      it.todo('should have a max durability value of 100');
+
+
+      describe('fail()', () => {
+            it('should ')
+      });
+
+      // describe('succeed()', () => {
+
+      // });
+
+      // describe('get()', () => {
+
+      // });
+})
+
+//fail 
+
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
